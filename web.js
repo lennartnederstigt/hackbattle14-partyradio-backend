2fae026a86e13bd10d8e7f4424017264fb72f452
Gino Mangnoesing <gvh.sing@gmail.com>
2014-04-23 18:36:54 +0200

    created public folder and made this publicly available, added background

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
-app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+app.use(express.logger());
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+app.get('/', function(req, res) {
+  res.sendfile('public/index.html');
+})
+
+app.use(express.static(__dirname + '/public'));
