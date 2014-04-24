540c16e04d70f5b06f3bd6873839a576eba49f00
Gino Mangnoesing <gvh.sing@gmail.com>
2014-04-24 13:46:26 +0200

    first commit of UI in public

---
+++ public/scripts/controllers/suggest.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('hackbattle14PartyradioFrontendApp')
+  .controller('SuggestCtrl', function ($scope, $http) {
+
+	$scope.$watch('suggestion', function(newValue, oldValue) {
+		if (newValue !== '' && newValue !== oldValue) {
+			$http.jsonp('http://api.deezer.com/search?q=' + encodeURIComponent(newValue) + '&limit=6&&output=jsonp&callback=JSON_CALLBACK').success(function(data) {
+				console.log(data);
+			    $scope.autocomplete = data.data
+			});
+
+		}
+
+	});
+
+	$scope.submit = function(deezerID) {
+		$http.get('http://tnw2014-appophetweb.herokuapp.com/api/song/' + deezerID);
+	}
+  });
